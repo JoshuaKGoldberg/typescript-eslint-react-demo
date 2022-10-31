d85cab54d17005ca7224d5b8f0b4ff880c1d3580
Josh Goldberg <git@joshuakgoldberg.com>
2022-10-30 23:50:30 -0400

    Spruced up demo a bit ✨

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,42 @@
 import Head from "next/head";
+import { useState } from "react";
 import { AwaitThenable } from "../components/AwaitThenable";
+import { FixedPromises } from "../components/Fixed";
 import { NoFloatingPromises } from "../components/NoFloatingPromises";
 import { NoMisusedPromises } from "../components/NoMisusedPromises";
 import styles from "../styles/Home.module.css";
 
-function exampleActionSync() {
-  document.body.style.backgroundColor = `rgb(${[
-    Math.random() * 255,
-    Math.random() * 255,
-    Math.random() * 255,
-  ].join()})`;
-}
+export default function Home() {
+  const [customColor, setCustomColor] = useState<string>();
 
-async function exampleActionAsync() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  exampleActionSync();
-}
+  function exampleActionSync() {
+    const color = `rgb(${[
+      Math.round(Math.random() * 255),
+      Math.round(Math.random() * 255),
+      Math.round(Math.random() * 255),
+    ].join(", ")})`;
 
-async function exampleActionAsyncRisky() {
-  if (Math.random() > 0.5) {
-    throw new Error("Gotcha!");
+    setCustomColor(color);
+
+    return color;
   }
 
-  await exampleActionAsync();
-}
+  async function exampleActionAsync() {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    exampleActionSync();
+  }
 
-export default function Home() {
+  async function exampleActionAsyncRisky() {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const color = exampleActionSync();
+
+    if (Math.random() > 0.5) {
+      throw new Error(`Gotcha: ${color}!`);
+    }
+  }
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>TypeScript-ESLint React Demo</title>
         <meta
@@ -37,26 +46,34 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>TypeScript-ESLint React Demo</h1>
-        <ol>
-          <li>
-            <AwaitThenable action={exampleActionSync}>
-              Violate <code>await-thenable</code>
-            </AwaitThenable>
-          </li>
-          <li>
-            <NoFloatingPromises action={exampleActionAsync}>
-              Violate <code>no-floating-promises</code>
-            </NoFloatingPromises>
-          </li>
-          <li>
-            <NoMisusedPromises action={exampleActionAsyncRisky}>
-              Violate <code>no-misused-promises</code>
-            </NoMisusedPromises>
-          </li>
-        </ol>
+      <main className={styles.main} style={{ background: customColor }}>
+        <div className={styles.contents}>
+          <h1 className={styles.title}>TypeScript-ESLint React Demo</h1>
+          <p aria-live="assertive">Custom color: {customColor ?? "(none)"}</p>
+          <ol>
+            <li>
+              <AwaitThenable action={exampleActionSync}>
+                Violate <code>await-thenable</code>
+              </AwaitThenable>
+            </li>
+            <li>
+              <NoFloatingPromises action={exampleActionAsync}>
+                Violate <code>no-floating-promises</code>
+              </NoFloatingPromises>
+            </li>
+            <li>
+              <NoMisusedPromises action={exampleActionAsyncRisky}>
+                Violate <code>no-misused-promises</code>
+              </NoMisusedPromises>
+            </li>
+            <li>
+              <FixedPromises action={exampleActionAsyncRisky}>
+                This may fail, safely
+              </FixedPromises>
+            </li>
+          </ol>
+        </div>
       </main>
-    </div>
+    </>
   );
 }
